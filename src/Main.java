216b98d54f760626ac0b78b8dc8bd0d0b3a57204
TimothyPadang <timothypadang22@gmail.com>
2026-05-27 21:34:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 import view.LoginFrame;
 import javax.swing.*;
 
-/**
- * Main - Entry Point Aplikasi
- * Sistem Manajemen Tugas dan Monitoring Deadline
- *
- * Fitur:
- * - 4 Pilar PBO: Enkapsulasi, Abstraksi, Polymorphism, Inheritance
- * - Multithreading: DeadlineMonitorThread
- * - GUI: Java Swing
- * - Database: MySQL via JDBC
- * - CRUD: Task, Category, User
- * - MVC Pattern
- */
 public class Main {
     public static void main(String[] args) {
         // Atur Look and Feel ke sistem
